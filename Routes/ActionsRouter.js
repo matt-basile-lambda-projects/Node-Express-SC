20693fbbe766c060548f6f67ca75d2c221f1cfea
Matt Basile <matthewbasile2@gmail.com>
2019-02-15 11:32:39 -0500

    Get requests live

--- Routes/ActionsRouter.js
+++ Routes/ActionsRouter.js
@@ -2,4 +2,19 @@ const express = require('express')
 const db = require('../data/helpers/actionModel.js')
 const router = express.Router();
 
+// GET REQUESTS
+router.get('/', (req, res) => {
+    db
+    .get()
+    .then(posts => {
+        if(posts){
+        res.status(200).json({success: true, posts})
+      } else{
+        res.status(500).json({success:false, message: 'The actions could not be retrieved.'});
+      }})
+      .catch(({code, message}) =>{
+        res.status(code).json({success: false, message})
+    })
+});
+
 module.exports = router
--- Routes/ProjectsRouter.js
+++ Routes/ProjectsRouter.js
@@ -2,4 +2,21 @@ const express = require('express')
 const db = require('../data/helpers/projectModel.js')
 const router = express.Router();
 
+// GET REQUESTS
+router.get('/', (req, res) => {
+    db
+    .get()
+    .then(posts => {
+        if(posts){
+        res.status(200).json({success: true, posts})
+      } else{
+        res.status(500).json({success:false, message: 'The projects could not be retrieved.'});
+      }})
+      .catch(({code, message}) =>{
+        res.status(code).json({success: false, message})
+    })
+});
+
+
+
 module.exports = router
